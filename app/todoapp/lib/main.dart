02191b6c743b3060deabb9ced736793e85cb0607
elisamongelli <81107849+elisamongelli@users.noreply.github.com>
2024-11-16 01:11:09 +0100

    Cleaning main file

--- app/todoapp/lib/main.dart
+++ app/todoapp/lib/main.dart
@@ -29,14 +29,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-      setState(() {
-        _counter++;
-      });
-  }
-
+  
   @override
   Widget build(BuildContext context) {
     
@@ -46,23 +39,7 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        
       ),
     );
   }
