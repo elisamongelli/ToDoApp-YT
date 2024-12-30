d3d1907eb66b9c58d1e7ee4ab7a01698b1ca6d8d
elisamongelli <81107849+elisamongelli@users.noreply.github.com>
2024-12-31 00:31:28 +0100

    POST method working for user Signup with request body

--- Backend/resources/signup.py
+++ Backend/resources/signup.py
@@ -1,3 +1,4 @@
+from flask import request
 from flask_restful import Resource
 
 
@@ -7,5 +8,33 @@ class Signup(Resource):
     def get(self):
         return {"message": "Get User for Signup!"}
     
+
     def post(self):
-        return {"message": "Post User for Signup!"}
+        
+        # request payload:
+            # {
+            #      "username" : "string",
+            #      "email" : "string",
+            #      "password" : "string"
+            # }
+        
+        json_data = request.get_json(force=True)
+        
+        print(json_data)
+
+        username = json_data["username"]
+        email = json_data["email"]
+        password = json_data["password"]
+
+        print("\n\nUsername = " + username +
+              "\nEmail = " + email + 
+              "\nPassword = " + password + "\n\n")
+
+        if not json_data:
+            return {"message": "No input data provided"}, 400
+
+        # TODO: need to check if username already exists
+        #   -redirect to signin
+        #   -choose another username
+
+        return {"message": "Create User {} for Signup!".format(username)}
