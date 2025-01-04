cd2d065e53f6240abf036a88738f6def09c3756b
elisamongelli <81107849+elisamongelli@users.noreply.github.com>
2025-01-04 19:36:19 +0100

    Trying to connect DB to the frontend: ID:#20

---
+++ app/todoapp/lib/models/authentication/authorize.dart
@@ -0,0 +1,42 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class AuthService with ChangeNotifier {
+  var currentUser;
+
+  AuthService() {
+    print("new AuthService");
+  }
+
+  Future getUser() {
+    return Future.value(currentUser);
+  }
+
+  // wrappinhg the firebase calls
+  Future logout() {
+    this.currentUser = null;
+    notifyListeners();
+    return Future.value(currentUser);
+  }
+
+  
+  /* Future createUser({
+      String username,
+      String email,
+      String password,
+      String firstName,
+      String lastName
+    }) async {} */
+
+  // logs in the user if password matches
+  /* Future loginUser({String email, String password}) {
+    if (password == 'password123') {
+      this.currentUser = {'email': email};
+      notifyListeners();
+      return Future.value(currentUser);
+    } else {
+      this.currentUser = null;
+      return Future.value(null);
+    }
+  } */
+}
