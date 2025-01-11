31ce46418e08deb1c11fd32818fc8b2798771f85
elisamongelli <81107849+elisamongelli@users.noreply.github.com>
2025-01-11 12:43:36 +0100

    Signup screen created with user creation on DB table: ID:#20

--- app/todoapp/lib/UI/Login/loginscreen.dart
+++ app/todoapp/lib/UI/Login/loginscreen.dart
@@ -1,3 +1,5 @@
+// import 'dart:js_interop';
+
 import 'package:flutter/material.dart';
 // import 'package:google_fonts/google_fonts.dart';
 import '../../models/assets/global.dart';
@@ -10,37 +12,78 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController firstNameController = TextEditingController();
+  TextEditingController lastNameController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: SafeArea(
-          child: Column(
-            children: <Widget>[
-              Text(
-                "Hi!",
-                style: loginScreenTitle
-              ),
-              Text(
-                "Ready to get productive?",
-                style: loginScreenSubtitle
-              ),
-              const TextField(
-
-              ),
-              FloatingActionButton.extended(
-                label: const Text("Sign up"),
-                backgroundColor: Colors.amber,
+        body: Center(
+            child: SafeArea(
+      child: Container(
+        margin: EdgeInsets.only(top: 20),
+        child: Column(
+          children: <Widget>[
+            Text("Hi!", style: loginScreenTitle),
+            Text("Ready to get productive?", style: loginScreenSubtitle),
+            Container(
+                margin:
+                    EdgeInsets.only(top: 60, left: 40, right: 40, bottom: 60),
+                child: Column(
+                  children: <Widget>[
+                    TextField(
+                      controller: firstNameController,
+                      decoration: InputDecoration(hintText: "First Name"),
+                    ),
+                    TextField(
+                      controller: lastNameController,
+                      decoration: InputDecoration(hintText: "Last name"),
+                    ),
+                    TextField(
+                      controller: emailController,
+                      decoration: InputDecoration(hintText: "Email"),
+                    ),
+                    TextField(
+                      controller: usernameController,
+                      decoration: InputDecoration(hintText: "Username"),
+                    ),
+                    TextField(
+                      controller: passwordController,
+                      decoration: InputDecoration(hintText: "Password"),
+                    ),
+                  ],
+                )),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: redMainColor,
                 foregroundColor: Colors.white,
-                shape: const OvalBorder(),
-                onPressed:() {
-                  bloc.signupUser("imateapot8", "teapot8@gmail.com", "teapot", "Elisa", "Mongelli");
-                },
               ),
-            ],
-          ),
-        )
-      )
-    );
+              onPressed: () {
+                bloc.signupUser(
+                  usernameController.text,
+                  emailController.text,
+                  passwordController.text,
+                  firstNameController.text,
+                  lastNameController.text
+                );
+              },
+              child: Text("SIGN UP DAJE"),
+            ),/* 
+            FloatingActionButton.extended(
+              label: const Text("Sign up"),
+              backgroundColor: Colors.amber,
+              foregroundColor: Colors.white,
+              shape: const OvalBorder(),
+              onPressed: () {
+                
+              },
+            ), */
+          ],
+        ),
+      ),
+    )));
   }
 }
