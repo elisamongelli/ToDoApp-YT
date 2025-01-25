7e1f8329dac827df7ca28bd51c7248965dd29b78
elisamongelli <elisamongelli00@gmail.com>
2025-01-25 11:04:30 +0100

    Continuing signup/login screen frontend

--- app/todoapp/lib/UI/Login/loginscreen.dart
+++ app/todoapp/lib/UI/Login/loginscreen.dart
@@ -1,3 +1,5 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import '../../models/assets/global.dart';
@@ -32,29 +34,11 @@ class LoginPageState extends State<LoginPage> {
 
     return Scaffold(
       body: Container(
-        color: lightGreyColor,
+        // color: Colors.white,
         child: Column(
           children: <Widget>[
-            Container(
-              // padding: const EdgeInsets.only(left: 30),
-              padding: const EdgeInsets.only(top: 60),
-              height: 320,
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(50),
-                  bottomRight: Radius.circular(50)
-                )
-              ),
-              child: Column(
-                // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text("Hi!", style: loginScreenTitle),
-                  Text("Ready to get productive?", style: loginScreenSubtitle),
-                  Container(),
-                ],
-              )
-            ),
+            Text("Hi!", style: loginScreenTitle),
+            Text("Ready to get productive?", style: loginScreenSubtitle),
             SingleChildScrollView(
               child: Column(
                 children: <Widget>[
